7f74d29487aea92f7018505b95923108f5f454ef
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-10-06 23:31:02 +0530

    Create LeapYear.java
    more changes will take place

---
+++ LeapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class LeapYear 
+{
+    public static void main(String[] args) 
+    {
+      int year,ex;
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.print("Enter a year: ");
+         year = sc.nextInt();
+
+        if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))	    //  quite understand 400 use for century and 4 for years and at end year divide by 
+         
+          System.out.println(year + " is a Leap Year.");
+
+        else
+         
+          System.out.println(year + " is not a Leap Year.");
+    }
+}
