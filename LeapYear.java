4b4569b0e06003d49c611ab20321d9573ac390f5
Cadet Adarsh <pal.adarsh10205@gmail.com>
2025-11-17 21:29:32 +0530

    Update LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -13,13 +13,14 @@ class LeapYear
          year = sc.nextInt();            
 
         if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))	    //  quite understand 400 use for century and 4 for years and at end year divide by 
-         
+        //{ 
           System.out.println(year + " is a Leap Year.");
-
+        //{
         else
          
           System.out.println(year + " is not a Leap Year.");
-        
+  ///  }
+//// }
     }
 }
 
