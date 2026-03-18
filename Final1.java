80f6bba3f7915d9c70bf4898392be145eacb5412
Cadet Adarsh <pal.adarsh10205@gmail.com>
2026-03-18 19:01:15 +0530

    Update Final1.java

--- Final1.java
+++ Final1.java
@@ -36,7 +36,7 @@ class Final1
 {
 	public static void main(String args[])
 	{
-		
+		 
 		two c = new two();						//	
 		//ramen c = new ramen();				// calling statement At first create a object and then call for function/method 
 		c.add(12,12);		
